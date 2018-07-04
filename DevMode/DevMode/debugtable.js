7c3bdbdbc1bd44b64993f06ad81c15be4991f419
SoonGames <soongames@gmx.de>
2018-07-04 23:56:34 +0200

    Die Sidebar ist jetzt der Container; Dadurch direkte Größenänderung

--- DevMode/DevMode/debugtable.js
+++ DevMode/DevMode/debugtable.js
@@ -5,30 +5,24 @@ function initDebugTable(css) {
 
     // Create DevMode-Sidebar
     $('body').append(
+        '<div class="devmode_sidebar_labels" id="devmode_sidebar_labels_debugtable"></div>' +
         '<div class="devmode_sidebar">' +
-            '<div class="devmode_sidebar_labels" id="devmode_sidebar_labels_debugtable"></div>' +
-            '<div class="devmode_sidebar_container">' +
-                '<div id="debugtable_names"></div>' +
-                '<div id="debugtable_attr"></div>' +
-            '</div>' +
+            '<div id="debugtable_names"></div>' +
+            '<div id="debugtable_attr"></div>' +
         '</div>'
     );
 
-    // Custom Stylesheet
-    $('.devmode_sidebar').css({
-        "height": "100%",
-        "width": "500px",
-        "top": "0px",
-        "right": "0px",
-        "display": "flex",
-        "position": "fixed",
-        "z-index": "1000"
+    $('#devmode_sidebar_labels_debugtable').css({
+        "background-image": "url('quest://res/DevMode/debugtable.png')"
     });
 
     $('.devmode_sidebar_labels').css({
         "box-sizing": "border-box",
         "text-align": "center",
         "border-bottom-left-radius": "10px",
+        "position": "absolute",
+        "top": "0px",
+        "right": "500px",
         "height": "50px",
         "width": "50px",
         "background-color": "ghostwhite",
@@ -36,54 +30,61 @@ function initDebugTable(css) {
         "background-position": "center",
         "box-shadow": "none",
         "transition": "box-shadow 0.5s",
-        "z-index": "0"
+        "z-index": "210"
     });
 
-    $('#devmode_sidebar_labels_debugtable').css({
-        "background-image": "url('quest://res/DevMode/debugtable.png')"
-    });
-
-    $('.devmode_sidebar_container').css({
+    $('.devmode_sidebar').css({
         "box-sizing": "border-box",
         "display": "flex",
         "height": "100%",
-        "width": "100%",
+        "width": "500px",
+        "top": "0px",
+        "right": "0px",
+        "display": "flex",
+        "position": "fixed",
+        "width": "500px",
         "border": "none",
         "box-shadow": "none",
-        "transition": "box-shadow 0.5s",
-        "background-color": "ghostwhite"
+        "transition": "box-shadow 0.2s",
+        "background-color": "ghostwhite",
+        "z-index": "200"
     });
 
     $('#debugtable_names').css({
         "box-sizing": "border-box",
         "float": "right",
         "height": "100%",
-        "width": "33.3%",
+        "width": "33.3%"
     });
     
     $('#debugtable_attr').css({
         "box-sizing": "border-box",
         "float": "right",
         "height": "100%",
-        "width": "66.6%",
+        "width": "66.6%"
     });
 
 
     // Declare variables
-    var devmode_sidebar_show = true;
-    var devmode_sidebar_boxshadow = "0px 0px 25px grey";
+    var devmode_sidebar_open = true;
+    var devmode_sidebar_boxshadow = "-5px 5px 10px rgba(0, 0, 0, 0.2)";
     
 
     // Resizing the sidebar
     $('.devmode_sidebar').resizable({
         handles:'w',
+        distance: 30,
+        minWidth: 300,
         resize: function (event,ui) {
             ui.position.left = ($(window).width() - $(this).width());
+            $('.devmode_sidebar_labels').css({ left: (ui.position.left - $('.devmode_sidebar_labels').width()) + "px" });
         },
         stop: function (event,ui) {
             $(this).css({ left: "initial" });
+            $('.devmode_sidebar_labels').css({ right: $(this).width(), left: "initial" });
         }
     });
+    $('.ui-resizable-w').css({ "width": "20px" }) // Width of the handle
 
 
     // Hidden DevMode-Sidebar
@@ -94,7 +95,7 @@ function initDebugTable(css) {
     $(".devmode_sidebar_labels").hover(function() {
         $(this).css("box-shadow", devmode_sidebar_boxshadow)
     }).mouseout(function() {
-        if (!devmode_sidebar_show) $(this).css("box-shadow","none")
+        if (!devmode_sidebar_open) $(this).css("box-shadow","none")
     });
 
 
@@ -106,8 +107,8 @@ function initDebugTable(css) {
 
     // DevMode-Sidebar fly in or out
     function toggletable (duration) {
-        devmode_sidebar_show = !devmode_sidebar_show;
-        if (devmode_sidebar_show) {
+        devmode_sidebar_open = !devmode_sidebar_open;
+        if (devmode_sidebar_open) {
             var tok = "+";
             var boxshadowval = devmode_sidebar_boxshadow;
             $('.devmode_sidebar').resizable("enable");
@@ -118,18 +119,18 @@ function initDebugTable(css) {
             var boxshadowval = "none";
             $('.devmode_sidebar').resizable("disable");
         }
-        $('.devmode_sidebar').animate({ right: tok + '=' + $('.devmode_sidebar_container').width() }, duration, "easeOutExpo" );
-        $('.devmode_sidebar_container').css('box-shadow', boxshadowval);
+        $('.devmode_sidebar, .devmode_sidebar_labels').animate({ right: tok + '=' + $('.devmode_sidebar').width() }, duration, "easeOutExpo" );
+        $('.devmode_sidebar').css('box-shadow', boxshadowval);
     }
 
 
     // If the window is resized, the sidebar should remain at the right margin
     $(window).resize(function() {
-        if (devmode_sidebar_show) {
+        if (devmode_sidebar_open) {
             $('.devmode_sidebar').css({ right: '0px' });
         }
         else {
-            $('.devmode_sidebar').css({ right: '-' + $('.devmode_sidebar_container').width() });
+            $('.devmode_sidebar').css({ right: '-' + $('.devmode_sidebar').width() });
         }
     });
     
