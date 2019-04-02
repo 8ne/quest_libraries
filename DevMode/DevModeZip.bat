cce0789df8bea2753f13a726d7365da3db8e205d
ScriptGames <kontakt@scriptgames.de>
2019-04-02 20:06:19 +0200

    DevMode 2.0
      Fixed a bug that prevented execution on the Internet.
      Sidebaricon was renewed.
      Sidebar is now transparent.

--- DevMode/DevModeZip.bat
+++ DevMode/DevModeZip.bat
@@ -1,3 +1,3 @@
 @echo off
-"%ProgramFiles%\7-Zip\7z.exe" a "DevMode.zip" "DevMode" "DevMode.aslx"
+"%ProgramFiles%\7-Zip\7z.exe" a "DevMode.zip" "DevMode.aslx" "devmode_sidebar.js" "jquery.sidebar.min.js" "tabulator.min.js"
 exit
